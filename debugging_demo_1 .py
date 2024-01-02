b99248fe87725b60d432513f75b0d7744c9df487
Ruchit Soni <ruchitsoni1898@gmail.com>
2024-01-02 16:38:15 +0100

    Initial commit

---
+++ debugging_demo_1 .py
@@ -0,0 +1,41 @@
+"""
+Official doc = https://docs.python.org/3/library/pdb.html
+c = continue
+n = next
+l = show current line
+s = step
+h = help
+pp = pretty print
+"""
+
+"""
+import pdb
+
+
+# demo 1
+print("I am the 1st line")
+fname = "Ruchit"
+lname = "Soni"
+
+pdb.set_trace()
+
+
+print("I am the 2nd line")
+print("I am the 3rd line")
+print(f"first name is '{fname}' and last name is '{lname}'")
+"""
+def get_user_name(name):
+    user_names = {"Ruchit": "Rs",
+                  "joe": "joe99",
+                  "mary": "marryrocks2020"}
+    print(f'The user "{user_names[name]}" is logged in.')
+    # pdb.set_trace()
+
+    return user_names[name]
+
+user_1 = get_user_name("Ruchit")
+print("User 1: " + user_1)
+# pdb.set_trace()
+
+user_2 = get_user_name("joe")
+print("User 2: " + user_2)
